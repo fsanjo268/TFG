86acfa757efd4b95dae5bcbfd83ce7f289fdbecb
Fernando San Jose <fsanjo268@gmail.com>
2024-04-22 02:48:08 +0200

    DMZ y eth2

--- Terraform/module-1/main.tf
+++ Terraform/module-1/main.tf
@@ -1,6 +1,6 @@
 resource "proxmox_lxc" "LXC_router" {
 
-  hostname = "test-vm-${count.index + 1}"
+  hostname = "test-lxc-router-${count.index + 1}"
   count = 1
   target_node = var.pm_node_name
   vmid = 100 + count.index
@@ -32,17 +32,21 @@ resource "proxmox_lxc" "LXC_router" {
     name = "eth1"
     bridge = "vmbr0"
     ip = "192.168.2.${count.index+100}/24"
-    gw = "192.168.1.1"
+  }
+  network {
+    name = "eth3"
+    bridge = "vmbr0"
+    ip = "192.168.3.${count.index+100}/24"
   }
   
 }
 
-resource "proxmox_lxc" "LXC_2" {
+resource "proxmox_lxc" "LXC_Atacante" {
 
-  hostname = "test-vm-${count.index + 2}"
+  hostname = "test-lxc-atacante-${count.index + 1}"
   count = 1
   target_node = var.pm_node_name
-  vmid = 101 + count.index
+  vmid = 200 + count.index
 
   ostemplate = "local:vztmpl/ubuntu-22.04-standard_22.04-1_amd64.tar.zst"
   cores = 1
@@ -62,9 +66,41 @@ resource "proxmox_lxc" "LXC_2" {
   }
   network {
     name = "eth0"
-    bridge = "eth1"
+    bridge = "vmbr0"
     ip = "192.168.2.${count.index+101}/24"
-    gw = "192.168.1.1"
+    gw = "192.168.2.100"
+  }
+  
+}
+
+resource "proxmox_lxc" "LXC_DMZ" {
+
+  hostname = "test-lxc-dmz-${count.index + 1}"
+  count = 1
+  target_node = var.pm_node_name
+  vmid = 300 + count.index
+
+  ostemplate = "local:vztmpl/ubuntu-22.04-standard_22.04-1_amd64.tar.zst"
+  cores = 1
+  memory = 512
+  start = true
+
+  unprivileged = false
+  password = var.lxc-password
+  ssh_public_keys = <<-EOT
+    ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKskyaMLTz3C97I8k1gGQLWu/oxrNfBESq241A68GCbN ansible@tfg2010
+    ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAlGhVOjcg+fhOQnF5UXgTfCJr2lTSRdmJO5zeDxcEbR root@tfg2010
+  EOT
+
+  rootfs {
+    storage = "local"
+    size = "2G"
+  }
+  network {
+    name = "eth0"
+    bridge = "vmbr0"
+    ip = "192.168.3.${count.index+101}/24"
+    gw = "192.168.3.100"
   }
   
 }
