af645ecea602f2c6abb7146c9b125e30e9440142
Fernando San Jose <fsanjo268@gmail.com>
2024-06-20 14:52:32 +0200

    fixed users creation users.sql

--- Auxiliar/MySQL/usuarios.sql
+++ Auxiliar/MySQL/usuarios.sql
@@ -1,4 +1,5 @@
 -- Crear usuario admin y otorgar permisos sobre la base de datos 'intranet'
+DROP USER IF EXISTS 'msantos'@'%';
 CREATE USER 'msantos'@'%' IDENTIFIED BY 'qwerty';
 GRANT ALL PRIVILEGES ON intranet.* TO 'msantos'@'%';
 FLUSH PRIVILEGES;
